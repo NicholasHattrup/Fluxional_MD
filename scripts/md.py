8a77af4a26f9f867ef1365582bf6d7862e09cb81
NicholasHattrup <nhattrup@andrew.cmu.edu>
2022-07-28 16:40:40 -0400

    Trajectories for bvl_bvl and bpin2

--- scripts/md.py
+++ scripts/md.py
@@ -6,9 +6,11 @@ from ase.md.langevin import Langevin
 from ase.md.velocitydistribution import Stationary, ZeroRotation
 from ase.io.trajectory import Trajectory as ASETrajectory
 import argparse
-
+import torch
+import time
 parser=argparse.ArgumentParser()
 parser.add_argument('--xyz', type=str,help='Path to xyz to construct ASE Atoms class from')
+parser.add_argument('--model',type=str,help='Path to nequip deployed model to run dynamics with')
 parser.add_argument('--temperature', type=float,help='Temperature to run dynamics at')
 parser.add_argument('--dt', type=float,help='Timestep for dynamics')
 parser.add_argument('--energy_units_to_eV', type=float, default=27.2114, help='model units -> eV factor')
@@ -16,20 +18,28 @@ parser.add_argument('--length_units_to_A', type=float, default=1, help='model un
 parser.add_argument('--num_steps', type=float, default=1000, help='Number of steps to run')
 parser.add_argument('--interval', type=float, default=10, help='Write current trajectory frame every n steps')
 parser.add_argument('--samples', type=int, default=1, help='Number of trajectories to sample/run')
+parser.add_argument('--dir',type=str,default='./',help='Path to write trajectory files to')
 #Define system
 args = parser.parse_args()
 
 atoms = read(args.xyz, index=0)
 init_xyz = atoms.positions.copy()
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 atoms.calc = NequIPCalculator.from_deployed_model(
-    model_path="../../../../bvl_bvl/models/bvl_bvl_rxn1_TS_MD.pth",
+    model_path=args.model,
+    device=device,
     species_to_type_name = {
         "H": "H",
         "B": "B",
 	"C": "C",
+        "N": "N",
 	"O": "O",
+        "F": "F",
+        "S": "S",
+        "Cl": "Cl",
+        "Br": "Br",
+        "I": "I"
     },
     energy_units_to_eV=args.energy_units_to_eV,
     length_units_to_A=args.length_units_to_A,
@@ -42,13 +52,15 @@ nvt_dyn = Langevin(
 	timestep=args.dt * units.fs,
 	friction=0.02)
 '''
-for i in range(97, args.samples):
+print('Running Trajectories using ' + str(device))
+start_time = time.time()
+for i in range(91,args.samples):
 	nvt_dyn = Langevin(
         atoms=atoms,
         temperature_K=args.temperature,
         timestep=args.dt * units.fs,
         friction=0.02)
-	traj_file = 'Trajectory_' + str(i) + '.traj'
+	traj_file = args.dir + '/' + 'Trajectory_' + str(i) + '.traj'
 	print(i, traj_file)
 	MaxwellBoltzmannDistribution(atoms=atoms, temp=args.temperature * units.kB)
 	ZeroRotation(atoms) # Set center of mass momentum to zero
@@ -61,4 +73,4 @@ for i in range(97, args.samples):
 	# reset atom positions to initial sampling geometry
 	atoms.set_positions(init_xyz.copy())
 
-
+print("--- %s seconds ---" % (time.time() - start_time))
